eda60a164122aba9495c3d4823a012b019fbbae7
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-09-13 20:59:23 +0530

    signup point with zod and bcrypt implementation

--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -1,18 +1,61 @@
 import  express , { Router } from "express";
+import { JWT_PASSWORD } from "../config.js";
+import jwt from "jsonwebtoken";
+import {z} from "zod";
+import { UserModel } from "../db.js";
+import bcrypt from "bcrypt";
+
 
 export const Userrouter = express.Router();
 
-import {z} from "zod";
+const signupSchema = z.object({
 
-const ZodSchema = z.object({
-    email:z.email({
-
-    })
+    email:z.email(),
+    firstName:z.string().min(1),
+    lastName:z.string().min(1),
+    password:z.string().min(6)
 
 })
 
+Userrouter.post("/signup" , async (req,res) => {
+    const body=req.body;
+    const parseSchema= signupSchema.safeParse(req.body);
+    if(!parseSchema.success){
+        return res.json({
+            message:"Incorrect Inputs"
+        })
+    }
 
-Userrouter.post("/signup" , (req,res)=>{
+    const { firstName, lastName, email, password } = parseSchema.data;
+
+    const existingUser = await UserModel.findOne({email});
+
+    if(existingUser){
+
+     return res.status(409).json({
+
+     message:"User already exists"
+   })
+}
+
+      
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const dbUser = await UserModel.create({
+        firstName,
+        lastName,
+        email,
+        password:hashedPassword
+     });
+
+     const token = jwt.sign({
+        userId:dbUser._id
+     }, JWT_PASSWORD)
+
+     res.json({
+        message:"user created successfully",
+        token:token
+     })
 
 })
 
